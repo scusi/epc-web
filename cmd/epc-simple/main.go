cbaa9e572ba23bb1c6e3f05b159de22c4bfeb933
Florian Walther <flw@scusi.io>
2022-03-15 21:49:41 +0100

    adding format flag and support to output as text or PNG

--- cmd/epc-simple/main.go
+++ cmd/epc-simple/main.go
@@ -4,7 +4,12 @@ import (
 	"gitlab.scusi.io/flow/epc"
 	"flag"
 	"fmt"
+	"log"
 )
+
+var debug bool
+//var out		string
+var format	string
 
 var bic		string
 var iban	string
@@ -15,6 +20,7 @@ var version     int
 var encoding    int
 
 func init() {
+	flag.BoolVar(&debug,      "debug", false, "prints debug messages, when true")
 	flag.Float64Var(&ammount, "a", 123.42,                   "ammount to transfer")
 	flag.StringVar(&bic,      "b", "COBADEFFXXX",            "BIC of the recipient")
 	flag.StringVar(&iban,     "i", "DE56120400000012262200", "IBAN of the recipient")
@@ -22,6 +28,8 @@ func init() {
 	flag.StringVar(&subject,  "s", "Test Überweisung",       "subject of the transfer")
 	flag.IntVar(&version,     "v", 2,                        "version of EPC, can be 1 or 2")
 	flag.IntVar(&encoding,    "e", 1,			 "encoding used, 1=UTF-8 2=ISO8859-1")
+	//flag.StringVar(&out,      "outfile", "-", "file to write output to")
+	flag.StringVar(&format,   "format", "text", "format to output, 'text', or 'png'")
 }
 
 func main() {
@@ -33,5 +41,18 @@ func main() {
 		subject,
 		ammount,
 	)
-	fmt.Printf("%s", e)
+	if debug {
+		log.Printf("%s", e)
+	}
+	switch format {
+	case "text":
+		fmt.Printf("%s", e)
+	case "png":
+		qr, err := e.MarshalQR()
+		if err != nil {
+			log.Println(err)
+		}
+		fmt.Printf("%s", qr)
+	}
+
 }
