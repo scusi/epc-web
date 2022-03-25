ec2ca2e1dd0395d32698297f1130fa98f4b38858
Florian Walther <flw@scusi.io>
2022-03-25 15:44:08 +0100

    enhanced regex to check subject and name to also include german umlaute and sharp s

--- cmd/epc-web/helper.go
+++ cmd/epc-web/helper.go
@@ -2,31 +2,31 @@ package main
 
 import (
 	"encoding/base64"
+	"fmt"
 	"gitlab.scusi.io/flow/epc"
-	"strconv"
 	"log"
 	"net/http"
 	"net/url"
 	"regexp"
-	"fmt"
+	"strconv"
 )
 
-func ibanIsValid(iban string) (bool) {
+func ibanIsValid(iban string) bool {
 	ibanRE := regexp.MustCompile(`^[A-Z]{2}[0-9A-Z]{2,32}$`)
 	return ibanRE.MatchString(iban)
 }
 
-func nameIsValid(name string) (bool) {
-	nameRE := regexp.MustCompile(`^[0-9A-Za-z\s\?\-\:\(\)\.\,\+\']{2,70}$`)
+func nameIsValid(name string) bool {
+	nameRE := regexp.MustCompile(`^[0-9A-Za-z\s\?\-\:\(\)\.\,\+\'üäöÜÄÖß]{2,70}$`)
 	return nameRE.MatchString(name)
 }
 
-func subjectIsValid(subject string) (bool) {
-	subjectRE := regexp.MustCompile(`^[0-9A-Za-z\s\?\-\:\(\)\.\,\+\']{1,140}$`)
+func subjectIsValid(subject string) bool {
+	subjectRE := regexp.MustCompile(`^[0-9A-Za-z\s\?\-\:\(\)\.\,\+\'üäöÜÄÖß]{1,140}$`)
 	return subjectRE.MatchString(subject)
 }
 
-func BICIsValid(bic string) (bool) {
+func BICIsValid(bic string) bool {
 	bicRE := regexp.MustCompile(`(?m)([a-zA-Z]{4})([a-zA-Z]{2})(([2-9a-zA-Z]{1})([0-9a-np-zA-NP-Z]{1}))((([0-9a-wy-zA-WY-Z]{1})([0-9a-zA-Z]{2}))|([xX]{3})|)`)
 	return bicRE.MatchString(bic)
 }
@@ -36,11 +36,13 @@ func urlparam2pD(r *http.Request) (pageData map[string]string) {
 	values := r.URL.Query()
 	for k, v := range values {
 		pageData[k] = v[0]
-		if debug { log.Printf("read parameter %s : %s", k, v)}
+		if debug {
+			log.Printf("read parameter %s : %s", k, v)
+		}
 	}
 	up := pD2URLparam(pageData)
 	pageData["epcurl"] = up
-	return	
+	return
 }
 
 func pD2URLparam(pageData map[string]string) (up string) {
@@ -64,20 +66,20 @@ func pD2epc(pageData map[string]string) (e *epc.EPC, err error) {
 	if ibanIsValid(pageData["epciban"]) != true {
 		err = fmt.Errorf("IBAN is invalid")
 		return
-    	}
+	}
 	if nameIsValid(pageData["epcname"]) != true {
 		err = fmt.Errorf("Name is invalid")
 		return
-    	}
+	}
 	if subjectIsValid(pageData["epcsubject"]) != true {
 		err = fmt.Errorf("Subject is invalid")
 		return
-    	}
+	}
 	if len(pageData["epcbic"]) > 0 {
 		if BICIsValid(pageData["epcbic"]) != true {
 			err = fmt.Errorf("BIC is invalid")
 			return
-		} 
+		}
 		e = epc.NewWithBIC(
 			pageData["epcbic"],
 			pageData["epcname"],
@@ -92,7 +94,7 @@ func pD2epc(pageData map[string]string) (e *epc.EPC, err error) {
 			pageData["epcsubject"],
 			amount,
 		)
-	} 
+	}
 	return
 }
 
